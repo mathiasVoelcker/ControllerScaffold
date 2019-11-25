c325055814b8b1ed06496fa834d689bd49b9ae0f
mathiasVoelckerPUCRS <mathias.voelcker@acad.pucrs.br>
2019-11-25 18:53:39 -0300

    scaffold finished

--- Files/DefaultValuesController.cs
+++ Files/DefaultValuesController.cs
@@ -7,9 +7,9 @@
 //     [Route("api/[controller]")]
 //     public class DefaultValuesController : ControllerBase
 //     {
-//         private IDefaultValueApplication _application;
+//         private object _application;
 
-//         public DefaultValuesController(IDefaultValueApplication application) {
+//         public DefaultValuesController(object application) {
 //             _application = applicaion;
 //         }
 
@@ -18,9 +18,27 @@
 //             return await _application.GetAll();
 //         }
 
+//         [HttpGet]
+//         public async Task<ActionResult<IList<DefaultValue>>> GetById(int id) {
+//             return await _application.GetById(id);
+//         }
+
+
 //         [HttpPost]
-//         public async Task<ActionResult> Create([FromBody]DefaultValue defaultValue) { 
+//         public async Task<ActionResult> Create([FromBody]object defaultValue) { 
 //             await _application.Create(defaultValue);
+//             return Ok("Default Value Successfully Registered");
+//         }
+
+//         [HttpPut]
+//         public async Task<ActionResult> Update([FromBody]object defaultValue) { 
+//             await _application.Update(defaultValue);
+//             return Ok("Default Value Successfully Registered");
+//         }
+
+//         [HttpDelete]
+//         public async Task<ActionResult> Delete([FromBody]object defaultValue) { 
+//             await _application.Delete(defaultValue);
 //             return Ok("Default Value Successfully Registered");
 //         }
 
--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using Extensions.MV;
 
 namespace DefaultApp
 {
@@ -17,6 +18,8 @@ namespace DefaultApp
 
                 Console.WriteLine(Directory.GetCurrentDirectory());
 
+                var controllerName = args[0];
+
                 var path = string.Concat("./Controllers/", args[0], "Controller", ".cs");
                 Console.WriteLine(path);
 
@@ -26,7 +29,13 @@ namespace DefaultApp
                 using (StreamWriter writer = new StreamWriter(path))  
                 {  
                     foreach(var line in ClassText()) {
-                        writer.WriteLine(line);
+                        if (!string.IsNullOrEmpty(controllerName)) {
+                            var newLine = line.Replace("Default", controllerName.Capitalize());
+                            newLine = newLine.Replace("default", controllerName.ToLower());
+                            writer.WriteLine(newLine);
+                        }     
+                        else 
+                            writer.WriteLine(line);
                     }
                 }
             } catch (Exception ex) {
@@ -39,29 +48,51 @@ namespace DefaultApp
         public static List<string> ClassText() {
             List<string> lines = new List<string>();
 //            using Microsoft.AspNetCore.Mvc;
+//     using System.Collections.Generic;
+// using System.Threading.Tasks;
+// using Microsoft.AspNetCore.Mvc;
             lines.Add("using Microsoft.AspNetCore.Mvc;");
+            lines.Add("using System.Collections.Generic;");
+            lines.Add("using System.Threading.Tasks;");
             lines.Add("");
             lines.Add("namespace DefaultProject.API.Controllers");
             lines.Add("{");
             lines.Add("");
             lines.Add("     [Route(\"api/[controller]\")]");
-            lines.Add("     [public class DefaultValuesController : ControllerBase");
+            lines.Add("     public class DefaultController : ControllerBase");
             lines.Add("     {");
-            lines.Add("         private IDefaultValueApplication _application;");
+            lines.Add("         private IDefaultApplication _application;");
             lines.Add("");
-            lines.Add("         public DefaultValuesController(IDefaultValueApplication application) {");
-            lines.Add("             _application = applicaion;");
+            lines.Add("         public DefaultController(DefaultApplication application) {");
+            lines.Add("             _application = application;");
             lines.Add("         }");
             lines.Add("");
             lines.Add("         [HttpGet]");
-            lines.Add("         public async Task<ActionResult<IList<DefaultValue>>> GetAll() {");
+            lines.Add("         public async Task<ActionResult<IList<Default>>> GetAll() {");
             lines.Add("             return await _application.GetAll();");
             lines.Add("         }");
             lines.Add("");
+            lines.Add("         [HttpGet]");
+            lines.Add("         public async Task<ActionResult<IList<Default>>> GetById() {");
+            lines.Add("             return await _application.GetById();");
+            lines.Add("         }");
+            lines.Add("");        
             lines.Add("         [HttpPost]");
-            lines.Add("         public async Task<ActionResult> Create([FromBody]DefaultValue defaultValue) {");
-            lines.Add("             return await _application.GetAll();");
-            lines.Add("             return Ok(\"Default Value Successfully Registered\")");
+            lines.Add("         public async Task<ActionResult> Create([FromBody]Default default) {");
+            lines.Add("             await _application.Create();");
+            lines.Add("             return Ok(\"Default Successfully Registered\");");
+            lines.Add("         }");
+            lines.Add("");        
+            lines.Add("         [HttpPut]");
+            lines.Add("         public async Task<ActionResult> Update([FromBody]Default default) {");
+            lines.Add("             await _application.Update();");
+            lines.Add("             return Ok(\"Default Successfully Updated\");");
+            lines.Add("         }");
+            lines.Add("");        
+            lines.Add("         [HttpDelete]");
+            lines.Add("         public async Task<ActionResult> Delete([FromBody]int id) {");
+            lines.Add("             await _application.Delete();");
+            lines.Add("             return Ok(\"Default Successfully Deleted\");");
             lines.Add("         }");
             lines.Add("     }");
             lines.Add("}");
